58900213ee293722b96596955a142682ae0131f1
rpdecks <rpdecks@gmail.com>
2020-05-12 16:50:04 -0400

    Done.

--- konami_code.js
+++ konami_code.js
@@ -12,5 +12,19 @@ const codes = [
 ];
 
 function init() {
-  // your code here
+  let i = 0;
+  document.body.addEventListener("keydown", (event) => {
+    let key = event.key;
+    if (key === codes[i]) {
+      console.log("correct")
+      i++;
+      if (i === codes.length) {
+        alert("Hurray");
+        index = 0;
+      }
+    } else {
+      console.log("wrong")
+      index = 0;
+    }
+  })
 }
